ee8dc5ec01ed2546362b56cfddc5fa096b8c6d70
pierledev <pierledev@gmail.com>
2024-04-23 16:44:23 +0700

    Change url on SocketContext

--- client/src/context/SocketContext.jsx
+++ client/src/context/SocketContext.jsx
@@ -16,7 +16,7 @@ export const SocketContextProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("https://chat-time-pierledev.onrender.com", {
+      const socket = io("https://chat-time-pierledev-j3uv.onrender.com", {
         query: {
           userId: authUser._id,
         },
